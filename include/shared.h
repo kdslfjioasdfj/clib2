acac75ec642104c34aa334f20b6cba2f7c8802de
IAMDARIZZLER SIGMA <IAMTHERIZZLER@yahoo.com>
2026-01-20 14:22:31 +0530

    Added a deprecation macro to include/shared.h

--- include/shared.h
+++ include/shared.h
@@ -1,6 +1,8 @@
 /**
  * @file shared.h
- * @brief Internal header for library visibility management
+ * @brief Internal Header: DO NOT USE
+ * @warning None of this is part of the API and is not intended for public usage
+ * @note This module may not be documented fully because it is not meant for public usage
  * @date 2025-12-26
  */
 
@@ -22,7 +24,18 @@
 #else
 // For other compilers, no special visibility handling (static libraries, etc.)
 #define CLIB2_SHARED_PUBLIC
-#endif
-#endif
+#endif // __GNUC__
+#endif // _WIN32
+
+// CLIB2_SHARED_DEPREC(MSG): Deprecated with message MSG
+#ifndef _WIN32
+#define CLIB2_SHARED_DEPREC(MSG) __declspec(deprecated(MSG))
+#else
+#if defined(__GNUC__) || defined(__clang__)
+#define CLIB2_SHARED_DEPREC(MSG) __attribute__((deprecated(MSG)))
+#else
+#define CLIB2_SHARED_DEPREC(MSG) /* No special treatment */
+#endif // __GNUC__ || __clang__
+#endif // _WIN32
 
 #endif // CLIB2_SHARED_H
